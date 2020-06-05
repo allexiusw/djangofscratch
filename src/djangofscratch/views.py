a07591f4d17a53d389f2f4485782799992f13329
William Alexander <william.al1379@gmail.com>
2020-06-05 01:23:36 -0600

    first commit added some routes and the first view

---
+++ src/djangofscratch/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse, Http404
+import datetime
+from django.template.loader import get_template
+from django.template import Context
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+def datetime_dynamic(request):
+    now = datetime.datetime.now()
+    html = "<html><body>%s</body></html>" % now
+    return HttpResponse(html)
+
+def add_hours(request, add):
+    try:
+        add = int(add)
+    except ValueError:
+        raise Http404()
+    now = datetime.datetime.now()
+    now_added = now + datetime.timedelta(add)
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'now': now, 'now_added':now_added, 'add':add}))
+    return HttpResponse(html)
+
