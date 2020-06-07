62791deccd5119453bea2c9904fae03818f78655
William Alexander <william.al1379@gmail.com>
2020-06-07 03:18:24 -0600

    enabled admin, added ordering and representation method to each of the models

--- src/djangofscratch/books/models.py
+++ src/djangofscratch/books/models.py
@@ -8,18 +8,29 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     website = models.URLField()
 
+    class Meta:
+        ordering = ['name']
+
     def __unicode__(self):
         return self.name
+
+    def __str__(self):
+        return self.name.title()
 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
+    
+    class Meta:
+        ordering = ['last_name']
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
 
+    def __str__(self):
+        return (self.last_name+" "+self.first_name).title()
 
 
 class Book(models.Model):
@@ -28,8 +39,11 @@ class Book(models.Model):
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     publication_date = models.DateField()
 
+    class Meta:
+        ordering = ['title']
+
     def __unicode__(self):
         return self.title
-
-
-
+    
+    def __str__(self):
+        return self.title.title()
