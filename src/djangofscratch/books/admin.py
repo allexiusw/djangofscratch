d7af80277cb1cac6677bd481c1b66f423f71329b
William Alexander <william.al1379@gmail.com>
2020-06-13 22:06:09 -0600

    Add search fields in django admin panel

--- src/djangofscratch/books/admin.py
+++ src/djangofscratch/books/admin.py
@@ -4,6 +4,7 @@ from djangofscratch.books.models import Publisher, Author, Book
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
 
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
