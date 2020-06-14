17e7ca88ef56b8c08e6e779994fa7270d597a0bb
William Alexander <william.al1379@gmail.com>
2020-06-13 21:53:44 -0600

    Add verbose name to email's Author field

--- src/djangofscratch/books/models.py
+++ src/djangofscratch/books/models.py
@@ -21,7 +21,7 @@ class Publisher(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    email = models.EmailField()
+    email = models.EmailField(verbose_name='e-mail')
     
     class Meta:
         ordering = ['last_name']
