6440f8b7302d00d1d765f1278a83f5e3663989ff
William Alexander <william.al1379@gmail.com>
2020-09-21 13:42:07 -0600

    add books view

--- src/djangofscratch/views.py
+++ src/djangofscratch/views.py
@@ -3,9 +3,20 @@ import datetime
 from django.shortcuts import render
 from django.template.loader import get_template
 from django.template import Context
+from djangofscratch.books.models import Book
 
 def home(request):
     return render(None, 'index.html', data)
 
 def books(request):
-    return render(None, 'index.html')
+    books = Book.objects.all()
+    data = {
+        'books_object' : books
+    }
+    q = request.META.get('q', "")
+    errors = []
+    if(q == ''):
+        errors.append('Enter a search term')
+    elif q.len > 20:
+        errors.append('Your search term has to be less than 20 characters')
+    return render(None, 'books.html', data)
